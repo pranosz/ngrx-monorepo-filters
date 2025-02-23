c202802060a5623465f11c851cdb67a5a54fbd22
Piotr Ranosz <pranosz@wp.pl>
2025-02-23 22:29:19 +0100

    app init

---
+++ libs/ui-kit/filters/src/lib/filters/filters.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-filters',
+  imports: [CommonModule],
+  templateUrl: './filters.component.html',
+  styleUrl: './filters.component.css',
+})
+export class FiltersComponent {}
