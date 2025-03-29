41e3fec2278b044291a6d325323c39824d6fec3e
Piotr Ranosz <pranosz@wp.pl>
2025-03-29 23:22:23 +0100

    filters

--- libs/products/src/lib/products/products.component.spec.ts
+++ libs/products/src/lib/products/products.component.spec.ts
@@ -1,5 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ProductsComponent } from './products.component';
+import { StoreModule } from '@ngrx/store';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
 
 describe('ProductsComponent', () => {
   let component: ProductsComponent;
@@ -7,7 +9,11 @@ describe('ProductsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ProductsComponent],
+      imports: [
+        ProductsComponent,
+        NoopAnimationsModule,
+        StoreModule.forRoot({})
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(ProductsComponent);
